1c01485e94397bbd462a85f97002692e1d9673d5
Scala <scala0114@gmail.com>
2022-12-14 21:01:31 +0900

    feat: solve +1 problem

---
+++ test_solutions/test_198.py
@@ -0,0 +1,25 @@
+import pytest
+from solutions.sol_198 import Solution
+
+cases = [
+    {
+        'input': {
+            'nums': [1, 2, 3, 1]
+        },
+        'output': 4
+    },
+    {
+        'input': {
+            'nums': [2, 7, 9, 3, 1]
+        },
+        'output': 12
+    },
+]
+
+
+@pytest.mark.sol198
+def test_run():
+    for case in cases:
+        assert Solution.rob(
+            nums=case['input']['nums']
+        ) == case['output']
