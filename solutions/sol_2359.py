aa0cdb341c7de5c3129a99f29c46bdc808016dee
Scala <scala0114@gmail.com>
2023-01-25 18:19:16 +0900

    feat: solve +1 problem

---
+++ solutions/sol_2359.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def closest_meeting_node(edges: List[int], node1: int, node2: int) -> int:
+        n = len(edges)
+        d1 = [-1] * n
+        d1[node1] = 0
+        node = node1
+        dst = 0
+        while edges[node] != -1:
+            dst += 1
+            nxt = edges[node]
+            if d1[nxt] == -1:
+                d1[nxt] = dst
+                node = nxt
+            else:
+                break
+        d2 = [-1] * n
+        d2[node2] = 0
+        node = node2
+        dst = 0
+        while edges[node] != -1:
+            dst += 1
+            nxt = edges[node]
+            if d2[nxt] == -1:
+                d2[nxt] = dst
+                node = nxt
+            else:
+                break
+
+        answer = -1
+        dmax = 10 ** 9
+        for i in range(n):
+            if d1[i] == -1 or d2[i] == -1:
+                continue
+            dm = max(d1[i], d2[i])
+            if dm < dmax:
+                answer, dmax = i, dm
+        return answer
---
+++ test_solutions/test_2359.py
@@ -0,0 +1,31 @@
+import pytest
+from solutions.sol_2359 import Solution
+
+cases = [
+    {
+        'input': {
+            'edges': [2, 2, 3, -1],
+            'node1': 0,
+            'node2': 1,
+        },
+        'output': 2
+    },
+    {
+        'input': {
+            'edges': [1, 2, -1],
+            'node1': 0,
+            'node2': 2,
+        },
+        'output': 2
+    },
+]
+
+
+@pytest.mark.sol_2359
+def test_run():
+    for case in cases:
+        assert Solution.closest_meeting_node(
+            edges=case['input']['edges'],
+            node1=case['input']['node1'],
+            node2=case['input']['node2'],
+        ) == case['output']
