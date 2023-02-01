d7447427c5dde3328170f487f0a495e38574152b
Scala <scala0114@gmail.com>
2023-02-01 20:53:35 +0900

    feat: solve +1 problem

---
+++ solutions/sol_1071.py
@@ -0,0 +1,13 @@
+class Solution:
+    @staticmethod
+    def gcd_of_strings(str1: str, str2: str) -> str:
+        if len(str1) > len(str2):
+            str1, str2 = str2, str1
+        while str1:
+            for i in range(len(str1)):
+                if str1[i] != str2[i]:
+                    return ""
+            str2 = str2[len(str1):]
+            if len(str1) > len(str2):
+                str1, str2 = str2, str1
+        return str2
---
+++ test_solutions/test_1071.py
@@ -0,0 +1,35 @@
+import pytest
+from solutions.sol_1071 import Solution
+
+cases = [
+    {
+        'input': {
+            'str1': 'ABCABC',
+            'str2': 'ABC',
+        },
+        'output': 'ABC'
+    },
+    {
+        'input': {
+            'str1': 'ABABAB',
+            'str2': 'ABAB',
+        },
+        'output': 'AB'
+    },
+    {
+        'input': {
+            'str1': 'LEET',
+            'str2': 'CODE',
+        },
+        'output': ''
+    },
+]
+
+
+@pytest.mark.sol1071
+def test_run():
+    for case in cases:
+        assert Solution.gcd_of_strings(
+            str1=case['input']['str1'],
+            str2=case['input']['str2'],
+        ) == case['output']
