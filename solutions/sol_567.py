d98a51e212e2334be38af5a7feb470db19cb5cd8
Scala <scala0114@gmail.com>
2023-02-04 13:53:05 +0900

    feat: solve +1 problem

---
+++ solutions/sol_567.py
@@ -0,0 +1,20 @@
+class Solution:
+    @staticmethod
+    def check_inclusion(s1: str, s2: str) -> bool:
+        n, m = len(s1), len(s2)
+        if n > m:
+            return False
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+        a_ord = ord('a')
+        for i in range(n):
+            s1_count[ord(s1[i]) - a_ord] += 1
+            s2_count[ord(s2[i]) - a_ord] += 1
+        if s1_count == s2_count:
+            return True
+        for i in range(m - n):
+            s2_count[ord(s2[n + i]) - a_ord] += 1
+            s2_count[ord(s2[i]) - a_ord] -= 1
+            if s1_count == s2_count:
+                return True
+        return False
