3b14de4c8393279f47d00dcf22c56efeb24ab50f
Scala <scala0114@gmail.com>
2023-02-05 12:10:44 +0900

    feat: solve +1 problem

---
+++ solutions/sol_438.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def find_anagrams(s: str, p: str) -> List[int]:
+        n, m = len(s), len(p)
+        if n < m:
+            return []
+        s_count = [0] * 26
+        p_count = [0] * 26
+        a_ord = ord('a')
+        for i in range(m):
+            s_count[ord(s[i]) - a_ord] += 1
+            p_count[ord(p[i]) - a_ord] += 1
+
+        answer = []
+        if s_count == p_count:
+            answer.append(0)
+        for i in range(n - m):
+            s_count[ord(s[m + i]) - a_ord] += 1
+            s_count[ord(s[i]) - a_ord] -= 1
+            if s_count == p_count:
+                answer.append(i + 1)
+        return answer
