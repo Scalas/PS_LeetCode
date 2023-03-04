a0e80be829eaa3123cde121cb031195f386b993c
Scala <scala0114@gmail.com>
2023-03-04 16:44:35 +0900

    feat: solve +1 problem

---
+++ solutions/sol_2444.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def count_sub_arrays(nums: List[int], min_k: int, max_k: int) -> int:
+        def count_case(sub_nums: List[int]):
+            min_index = []
+            max_index = []
+            for i in range(len(sub_nums)):
+                if sub_nums[i] == min_k:
+                    min_index.append(i)
+                if sub_nums[i] == max_k:
+                    max_index.append(i)
+            if not min_index or not max_index:
+                return 0
+            max_start = min(min_index[-1], max_index[-1])
+            res = 0
+            for start in range(max_start + 1):
+                min_end = max(min_index[bisect_left(min_index, start)], max_index[bisect_left(max_index, start)])
+                res += (len(sub_nums) - min_end)
+            return res
+
+        answer = 0
+        sub = []
+        for i in range(len(nums)):
+            if min_k <= nums[i] <= max_k:
+                sub.append(nums[i])
+            else:
+                if sub:
+                    answer += count_case(sub)
+                sub = []
+        if sub:
+            answer += count_case(sub)
+        return answer
---
+++ test_solutions/test_2444.py
@@ -0,0 +1,40 @@
+import pytest
+from solutions.sol_2444 import Solution
+
+cases = [
+    {
+        'input': {
+            'nums': [1, 3, 5, 2, 7, 5],
+            'min_k': 1,
+            'max_k': 5,
+        },
+        'output': 2,
+    },
+    {
+        'input': {
+            'nums': [1, 1, 1, 1],
+            'min_k': 1,
+            'max_k': 1,
+        },
+        'output': 10,
+    },
+    {
+        'input': {
+            'nums': [689862, 297861, 946099, 25145, 946099, 647669, 863241, 886257, 946099, 25145, 567132, 484586,
+                     478308, 427044, 545054, 25145, 25145, 25145, 25145, 25145],
+            'min_k': 25145,
+            'max_k': 946099,
+        },
+        'output': 122,
+    },
+]
+
+
+@pytest.mark.sol_2444
+def test_run():
+    for case in cases:
+        assert Solution.count_sub_arrays(
+            nums=case['input']['nums'],
+            min_k=case['input']['min_k'],
+            max_k=case['input']['max_k']
+        ) == case['output']
