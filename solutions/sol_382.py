0b166a0c482343758598653127bca0f26c534c32
Scala <scala0114@gmail.com>
2023-03-10 21:52:13 +0900

    feat: solve +1 problem

---
+++ solutions/sol_382.py
@@ -0,0 +1,17 @@
+from random import randint
+from typing import Optional
+
+from leet_code_types.leet_code_types import ListNode
+
+
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.nums = []
+        while head:
+            self.nums.append(head.val)
+            head = head.next
+        self.size = len(self.nums)
+
+    def get_random(self) -> int:
+        return self.nums[randint(0, self.size - 1)]
