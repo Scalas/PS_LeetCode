8a381d3732998adaa37651aa8917d3e701d053dd
Scala <scala0114@gmail.com>
2023-03-19 15:07:13 +0900

    feat: solve +1 problem

---
+++ solutions/sol_211.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, val='', is_term=False):
+        self.__val = val
+        self.__is_term = is_term
+        self.__children = dict()
+
+    def get_child(self, alpha):
+        return self.__children.get(alpha, None)
+
+    def get_children(self):
+        return self.__children.values()
+
+    def add_child(self, child):
+        self.__children[child.__val] = child
+
+    def mark_terminal(self):
+        self.__is_term = True
+
+    def is_terminal(self):
+        return self.__is_term
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.__root = Node()
+
+    def addWord(self, word: str) -> None:
+        pos = self.__root
+        idx = 0
+        while idx < len(word):
+            child = pos.get_child(word[idx])
+            if not child:
+                break
+            pos = child
+            idx += 1
+        while idx < len(word):
+            new_node = Node(word[idx], False)
+            pos.add_child(new_node)
+            pos = new_node
+            idx += 1
+        pos.mark_terminal()
+
+    def search(self, word: str) -> bool:
+        return self.__search(self.__root, word, 0)
+
+    def __search(self, node, word, idx):
+        if idx == len(word):
+            return node.is_terminal()
+        if word[idx] == '.':
+            for child in node.get_children():
+                if self.__search(child, word, idx + 1):
+                    return True
+            return False
+        else:
+            child = node.get_child(word[idx])
+            if not child:
+                return False
+            return self.__search(child, word, idx + 1)
---
+++ test_solutions/test_211.py
@@ -0,0 +1,32 @@
+import pytest
+from solutions.sol_211 import WordDictionary
+
+cases = [
+    {
+        'input': {
+            'commands': ["WordDictionary", "addWord", "addWord", "addWord", "search", "search", "search", "search"],
+            'args': [[], ["bad"], ["dad"], ["mad"], ["pad"], ["bad"], [".ad"], ["b.."]]
+        },
+        'output': [None, None, None, None, False, True, True, True]
+    },
+]
+
+
+@pytest.mark.sol211
+def test_run():
+    for case in cases:
+        commands = case['input']['commands']
+        args = case['input']['args']
+
+        w = WordDictionary()
+        answer = [None]
+        for i in range(1, len(commands)):
+            if commands[i] == 'addWord':
+                w.addWord(args[i][0])
+                answer.append(None)
+                continue
+            if commands[i] == 'search':
+                answer.append(w.search(args[i][0]))
+                continue
+
+        assert answer == case['output']
