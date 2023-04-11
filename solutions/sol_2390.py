19aa4150616c0d95eb3a6f330396596b800e8716
Scala <scala0114@gmail.com>
2023-04-11 20:10:04 +0900

    feat: solve +1 problem

---
+++ solutions/sol_2390.py
@@ -0,0 +1,11 @@
+class Solution:
+    @staticmethod
+    def remove_stars(s: str) -> str:
+        sq = []
+        for c in s:
+            if c == '*':
+                if sq:
+                    sq.pop()
+            else:
+                sq.append(c)
+        return ''.join(sq)
---
+++ test_solutions/test_2390.py
@@ -0,0 +1,25 @@
+import pytest
+from solutions.sol_2390 import Solution
+
+cases = [
+    {
+        'input': {
+            's': 'leet**cod*e',
+        },
+        'output': 'lecoe',
+    },
+    {
+        'input': {
+            's': 'erase*****',
+        },
+        'output': '',
+    },
+]
+
+
+@pytest.mark.sol_2390
+def test_run():
+    for case in cases:
+        assert Solution.remove_stars(
+            s=case['input']['s'],
+        ) == case['output']
