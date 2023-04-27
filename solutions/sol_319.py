4594b5c8764cf8e44c212679e6a3b3b31ba1f5ef
Scala <scala0114@gmail.com>
2023-04-27 21:12:36 +0900

    feat: solve +1 problem

---
+++ solutions/sol_319.py
@@ -0,0 +1,4 @@
+class Solution:
+    @staticmethod
+    def bulb_switch(n: int) -> int:
+        return int(n**.5)
---
+++ test_solutions/test_319.py
@@ -0,0 +1,31 @@
+import pytest
+from solutions.sol_319 import Solution
+
+cases = [
+    {
+        'input': {
+            'n': 3
+        },
+        'output': 1
+    },
+    {
+        'input': {
+            'n': 0
+        },
+        'output': 0
+    },
+    {
+        'input': {
+            'n': 1
+        },
+        'output': 1
+    },
+]
+
+
+@pytest.mark.sol_319
+def test_run():
+    for case in cases:
+        assert Solution.bulb_switch(
+            n=case['input']['n']
+        ) == case['output']
