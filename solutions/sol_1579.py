aa4473c91b465a2b6a924b98f13ad9abc722aeca
Scala <scala0114@gmail.com>
2023-04-30 14:43:49 +0900

    feat: solve +1 problem

---
+++ solutions/sol_1579.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def max_num_edges_to_remove(n: int, edges: List[List[int]]) -> int:
+        au = [-1] * (n + 1)
+        bu = [-1] * (n + 1)
+        au[0] = bu[0] = 0
+
+        def union(u, a, b):
+            a = find(u, a)
+            b = find(u, b)
+            if a != b:
+                if u[a] < u[b]:
+                    u[a] += u[b]
+                    u[b] = a
+                else:
+                    u[b] += u[a]
+                    u[a] = b
+                return True
+            else:
+                return False
+
+        def find(u, x):
+            if u[x] < 0:
+                return x
+            u[x] = find(u, u[x])
+            return u[x]
+
+        connect = 0
+        edges.sort(key=lambda x: -x[0])
+        for t, o1, o2 in edges:
+            connected = False
+            if t % 2:
+                connected = union(au, o1, o2)
+            if t > 1:
+                connected = union(bu, o1, o2)
+            if connected:
+                connect += 1
+
+        if sum(map(lambda x: 1 if x < 0 else 0, au)) > 1 or sum(map(lambda x: 1 if x < 0 else 0, bu)) > 1:
+            return -1
+
+        return len(edges) - connect
---
+++ test_solutions/test_1579.py
@@ -0,0 +1,42 @@
+import pytest
+from solutions.sol_1579 import Solution
+
+cases = [
+    {
+        'input': {
+            'n': 4,
+            'edges': [[3, 1, 2], [3, 2, 3], [1, 1, 3], [1, 2, 4], [1, 1, 2], [2, 3, 4]],
+        },
+        'output': 2,
+    },
+    {
+        'input': {
+            'n': 4,
+            'edges': [[3, 1, 2], [3, 2, 3], [1, 1, 4], [2, 1, 4]],
+        },
+        'output': 0,
+    },
+    {
+        'input': {
+            'n': 4,
+            'edges': [[3, 2, 3], [1, 1, 2], [2, 3, 4]],
+        },
+        'output': -1,
+    },
+    {
+        'input': {
+            'n': 2,
+            'edges': [[1, 1, 2], [2, 1, 2], [3, 1, 2]],
+        },
+        'output': 2,
+    },
+]
+
+
+@pytest.mark.sol1579
+def test_run():
+    for case in cases:
+        assert Solution.max_num_edges_to_remove(
+            n=case['input']['n'],
+            edges=case['input']['edges'],
+        ) == case['output']
