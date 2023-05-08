0c287725c1f6d62c1819d6aef184e77f70acf1a6
Scala <scala0114@gmail.com>
2023-05-08 21:25:14 +0900

    feat: solve +1 problem

---
+++ solutions/sol_1572.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def diagonal_sum(mat: List[List[int]]) -> int:
+        n = len(mat)
+        answer = 0
+        for i in range(n):
+            answer += (mat[i][i] + mat[i][n - 1 - i])
+        if n % 2:
+            mid = n // 2
+            answer -= mat[mid][mid]
+        return answer
---
+++ test_solutions/test_1572.py
@@ -0,0 +1,36 @@
+import pytest
+from solutions.sol_1572 import Solution
+
+cases = [
+    {
+        'input': {
+            'mat': [[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]],
+        },
+        'output': 25
+    },
+    {
+        'input': {
+            'mat': [[1, 1, 1, 1],
+                    [1, 1, 1, 1],
+                    [1, 1, 1, 1],
+                    [1, 1, 1, 1]]
+        },
+        'output': 8
+    },
+    {
+        'input': {
+            'mat': [[5]],
+        },
+        'output': 5
+    },
+]
+
+
+@pytest.mark.sol1572
+def test_run():
+    for case in cases:
+        assert Solution.diagonal_sum(
+            mat=case['input']['mat'],
+        ) == case['output']
