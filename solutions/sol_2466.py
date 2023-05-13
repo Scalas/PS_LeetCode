d1120ff7f94fe0b66ecab25dce2f8c7b6f3207bf
Scala <scala0114@gmail.com>
2023-05-13 12:46:42 +0900

    feat: solve +1 problem

---
+++ solutions/sol_2466.py
@@ -0,0 +1,17 @@
+class Solution:
+    @staticmethod
+    def count_good_strings(low: int, high: int, zero: int, one: int) -> int:
+        answer = 0
+        mod = 10 ** 9 + 7
+        dp = [-1] * (high + 1)
+        dp[0] = 1
+        for i in range(1, high + 1):
+            res = 0
+            if i - zero >= 0:
+                res = (res + dp[i - zero]) % mod
+            if i - one >= 0:
+                res = (res + dp[i - one]) % mod
+            dp[i] = res
+            if low <= i <= high:
+                answer = (answer + res) % mod
+        return answer
---
+++ test_solutions/test_2466.py
@@ -0,0 +1,34 @@
+import pytest
+from solutions.sol_2466 import Solution
+
+cases = [
+    {
+        'input': {
+            'low': 3,
+            'high': 3,
+            'zero': 1,
+            'one': 1,
+        },
+        'output': 8,
+    },
+    {
+        'input': {
+            'low': 2,
+            'high': 3,
+            'zero': 1,
+            'one': 2,
+        },
+        'output': 5,
+    },
+]
+
+
+@pytest.mark.sol_2466
+def test_run():
+    for case in cases:
+        assert Solution.count_good_strings(
+            low=case['input']['low'],
+            high=case['input']['high'],
+            zero=case['input']['zero'],
+            one=case['input']['one']
+        ) == case['output']
