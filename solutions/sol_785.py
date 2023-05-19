a7468d0dd6101e6ca834bf431772f0561721debe
Scala <scala0114@gmail.com>
2023-05-19 21:00:52 +0900

    feat: solve +1 problem

---
+++ solutions/sol_785.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def is_bipartite(graph: List[List[int]]) -> bool:
+
+        n = len(graph)
+        group = [-1] * n
+
+        def dfs(cur, group_id):
+            if group[cur] == group_id:
+                return True
+            if group[cur] == -1:
+                group[cur] = group_id
+                for nxt in graph[cur]:
+                    if not dfs(nxt, 1 - group_id):
+                        return False
+                return True
+            else:
+                return False
+
+        for i in range(n):
+            if group[i] == -1:
+                if not dfs(i, 0):
+                    return False
+        return True
---
+++ test_solutions/test_785.py
@@ -0,0 +1,25 @@
+import pytest
+from solutions.sol_785 import Solution
+
+cases = [
+    {
+        'input': {
+            'graph': [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]],
+        },
+        'output': False
+    },
+    {
+        'input': {
+            'graph': [[1, 3], [0, 2], [1, 3], [0, 2]],
+        },
+        'output': True
+    },
+]
+
+
+@pytest.mark.sol_785
+def test_run():
+    for case in cases:
+        assert Solution.is_bipartite(
+            graph=case['input']['graph'],
+        ) == case['output']
