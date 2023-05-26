6f321b594466e73a8d6bbf2c149d067db4e489f7
Scala <scala0114@gmail.com>
2023-05-26 13:39:37 +0900

    feat: solve +1 problem

---
+++ solutions/sol_1140.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def stone_game_2(piles: List[int]) -> int:
+        n = len(piles)
+        for i in range(1, n):
+            piles[i] += piles[i - 1]
+        piles.append(0)
+        dp = [[[-1] * 2 for _ in range(100)] for _ in range(n)]
+
+        def dfs(cur, m, turn):
+            if cur == n:
+                return 0
+            if dp[cur][m][turn] == -1:
+                res = 0
+                if turn:
+                    res = 10 ** 9
+                    for i in range(m * 2):
+                        nxt = cur + i + 1
+                        if nxt > n:
+                            break
+                        res = min(res, dfs(nxt, max(m, i + 1), 1 - turn))
+                else:
+                    for i in range(m * 2):
+                        nxt = cur + i + 1
+                        if nxt > n:
+                            break
+                        res = max(res, dfs(nxt, max(m, i + 1), 1 - turn) + piles[nxt - 1] - piles[cur - 1])
+                dp[cur][m][turn] = res
+            return dp[cur][m][turn]
+        answer = dfs(0, 1, 0)
+        return answer
