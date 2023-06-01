5ca2d19c5be6f0189ba3875b6a74df94ca6f11e9
Scala <scala0114@gmail.com>
2023-06-01 21:12:56 +0900

    feat: solve +1 problem

---
+++ solutions/sol_1091.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def shortest_path_binary_matrix(grid: List[List[int]]) -> int:
+        if grid[0][0]:
+            return -1
+
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        n = len(grid)
+        if n == 1:
+            return 1
+
+        visited = [[False] * n for _ in range(n)]
+        q = [(0, 0)]
+        visited[0][0] = True
+        turn = 1
+        while q:
+            turn += 1
+            nq = []
+            for cr, cc in q:
+                for dr, dc in directions:
+                    nr, nc = cr + dr, cc + dc
+                    if not (0 <= nr < n and 0 <= nc < n):
+                        continue
+                    if grid[nr][nc] == 1:
+                        continue
+                    if nr == nc == n - 1:
+                        return turn
+                    if visited[nr][nc]:
+                        continue
+                    visited[nr][nc] = True
+                    nq.append((nr, nc))
+            q = nq
+        return -1
