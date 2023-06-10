2c4c20ee7651d4a07165232bde8b7ef9ac89e2bb
Scala <scala0114@gmail.com>
2023-06-10 12:28:05 +0900

    feat: sovle +1 problem

---
+++ test_solutions/test_1802.py
@@ -0,0 +1,39 @@
+import pytest
+from solutions.sol_1802 import Solution
+
+cases = [
+    {
+        'input': {
+            'n': 4,
+            'index': 2,
+            'max_sum': 6,
+        },
+        'output': 2,
+    },
+    {
+        'input': {
+            'n': 6,
+            'index': 1,
+            'max_sum': 10,
+        },
+        'output': 3,
+    },
+    {
+        'input': {
+            'n': 2,
+            'index': 1,
+            'max_sum': 21,
+        },
+        'output': 11,
+    },
+]
+
+
+@pytest.mark.sol1802
+def test_run():
+    for case in cases:
+        assert Solution.max_value(
+            n=case['input']['n'],
+            index=case['input']['index'],
+            max_sum=case['input']['max_sum']
+        ) == case['output']
