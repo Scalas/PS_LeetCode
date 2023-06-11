babf8fd1e1c70b8f7fdbc52fbb1dbd89b99092d8
Scala <scala0114@gmail.com>
2023-06-11 17:53:47 +0900

    feat: solve +1 problem

---
+++ solutions/sol_1146.py
@@ -0,0 +1,27 @@
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.snapshot = [dict()]
+        self.dirty = 0
+
+    def set(self, index: int, val: int) -> None:
+        self.snapshot[-1][index] = val
+        self.dirty = 1
+
+    def snap(self) -> int:
+        if not self.dirty and len(self.snapshot) > 1:
+            self.snapshot[-1] = self.snapshot[-2]
+        self.snapshot.append(dict())
+        self.dirty = 0
+        return len(self.snapshot) - 2
+
+    def get(self, index: int, snap_id: int) -> int:
+        def find(sid, idx):
+            if sid == -1:
+                return 0
+            val = self.snapshot[sid].get(idx, -1)
+            if val != -1:
+                return val
+            self.snapshot[sid][idx] = find(sid - 1, idx)
+            return self.snapshot[sid][idx]
+        return find(snap_id, index)
---
+++ test_solutions/test_1146.py
@@ -0,0 +1,32 @@
+import pytest
+from solutions.sol_1146 import SnapshotArray
+
+cases = [
+    {
+        'input': {
+            'commands': ["SnapshotArray", "set", "snap", "set", "get"],
+            'args': [[3], [0, 5], [], [0, 6], [0, 0]],
+        },
+        'output': [None, None, 0, None, 5],
+    },
+]
+
+
+@pytest.mark.sol1146
+def test_run():
+    for case in cases:
+        commands = case['input']['commands']
+        args = case['input']['args']
+
+        sa = SnapshotArray(args[0][0])
+        res = [None]
+        for i in range(1, len(commands)):
+            cmd = commands[i]
+            arg = args[i]
+            if cmd == 'set':
+                res.append(sa.set(arg[0], arg[1]))
+            elif cmd == 'snap':
+                res.append(sa.snap())
+            else:
+                res.append(sa.get(arg[0], arg[1]))
+        assert res == case['output']
