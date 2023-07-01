9b3b150bed61dfb82ca32db4f6d9a4041c939fa0
Scala <scala0114@gmail.com>
2023-07-01 13:01:17 +0900

    feat: solve +1 problem

---
+++ solutions/sol_2305.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def distribute_cookies(cookies: List[int], k: int) -> int:
+        n = len(cookies)
+        answer = sum(sorted(cookies)[k - 1:])
+
+        def dfs(cur, bags):
+            nonlocal answer
+            if cur == n:
+                answer = min(answer, max(bags))
+                return
+            for i in range(k):
+                nsize = bags[i] + cookies[cur]
+                if nsize >= answer:
+                    continue
+                back_up = bags[i]
+                bags[i] = nsize
+                dfs(cur + 1, bags)
+                bags[i] = back_up
+
+        dfs(0, [0] * k)
+        return answer
---
+++ test_solutions/test_2305.py
@@ -0,0 +1,28 @@
+import pytest
+from solutions.sol_2305 import Solution
+
+cases = [
+    {
+        'input': {
+            'cookies': [8, 15, 10, 20, 8],
+            'k': 2,
+        },
+        'output': 31,
+    },
+    {
+        'input': {
+            'cookies': [6, 1, 3, 2, 2, 4, 1, 2],
+            'k': 3,
+        },
+        'output': 7,
+    },
+]
+
+
+@pytest.mark.sol_2305
+def test_run():
+    for case in cases:
+        assert Solution.distribute_cookies(
+            cookies=case['input']['cookies'],
+            k=case['input']['k'],
+        ) == case['output']
