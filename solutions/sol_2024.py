7ea091c2e4bf6cbfce3c272e069f1ce785666466
Scala <scala0114@gmail.com>
2023-07-08 00:06:33 +0900

    feat: solve +1 problem

---
+++ solutions/sol_2024.py
@@ -0,0 +1,35 @@
+class Solution:
+    @staticmethod
+    def max_consecutive_answers(answer_key: str, k: int) -> int:
+        n = len(answer_key)
+        tacc, facc = [], []
+        tc, fc = 0, 0
+        for v in answer_key:
+            if v == 'T':
+                tc += 1
+            else:
+                fc += 1
+            tacc.append(tc)
+            facc.append(fc)
+        tacc.append(0)
+        facc.append(0)
+
+        def check(val):
+            for i in range(val - 1, n):
+                # fill with T
+                if facc[i] - facc[i - val] <= k:
+                    return True
+                if tacc[i] - tacc[i - val] <= k:
+                    return True
+            return False
+
+        s, e = 0, n + 1
+        answer = 0
+        while s <= e:
+            mid = (s + e) // 2
+            if check(mid):
+                answer = max(answer, mid)
+                s = mid + 1
+            else:
+                e = mid - 1
+        return answer
---
+++ test_solutions/test_2024.py
@@ -0,0 +1,35 @@
+import pytest
+from solutions.sol_2024 import Solution
+
+cases = [
+    {
+        'input': {
+            'answer_key': 'TTFF',
+            'k': 2,
+        },
+        'output': 4,
+    },
+    {
+        'input': {
+            'answer_key': 'TFFT',
+            'k': 1,
+        },
+        'output': 3,
+    },
+    {
+        'input': {
+            'answer_key': 'TTFTTFTT',
+            'k': 1,
+        },
+        'output': 5,
+    },
+]
+
+
+@pytest.mark.sol_2024
+def test_run():
+    for case in cases:
+        assert Solution.max_consecutive_answers(
+            answer_key=case['input']['answer_key'],
+            k=case['input']['k'],
+        ) == case['output']
