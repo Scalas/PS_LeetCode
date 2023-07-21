85d24aba5a81e98e4df114175de71a0d641a4ede
Scala <scala0114@gmail.com>
2023-07-22 00:10:51 +0900

    feat: solve +1 problem

---
+++ solutions/sol_673.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+
+
+class Solution:
+    @staticmethod
+    def find_number_of_lis(nums: List[int]) -> int:
+        n = len(nums)
+        minus = 10 ** 6
+        nums = list(map(lambda x: x + minus, nums))
+
+        def dfs(cur, last) -> Tuple[int, int]:
+            if cur == n:
+                return 0, 1
+            if dp[cur].get(last, -1) == -1:
+                length, count = dfs(cur + 1, last)
+                if nums[cur] > last:
+                    nl, nc = dfs(cur + 1, nums[cur])
+                    nl += 1
+                    if nl > length:
+                        length, count = nl, nc
+                    elif nl == length:
+                        count += nc
+                dp[cur][last] = length, count
+            return dp[cur][last]
+
+        dp = [dict() for _ in range(n)]
+        return dfs(0, 0)[1]
---
+++ test_solutions/test_673.py
@@ -0,0 +1,31 @@
+import pytest
+from solutions.sol_673 import Solution
+
+cases = [
+    {
+        'input': {
+            'nums': [1, 3, 5, 4, 7],
+        },
+        'output': 2,
+    },
+    {
+        'input': {
+            'nums': [2, 2, 2, 2, 2],
+        },
+        'output': 5,
+    },
+    {
+        'input': {
+            'nums': [-100, -99, -98, -97, -96, -96],
+        },
+        'output': 2,
+    },
+]
+
+
+@pytest.mark.sol_673
+def test_run():
+    for case in cases:
+        assert Solution.find_number_of_lis(
+            nums=case['input']['nums'],
+        ) == case['output']
