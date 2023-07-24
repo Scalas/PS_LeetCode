198efff78255ceddea737d1a63eef6fd492d0cc4
Scala <scala0114@gmail.com>
2023-07-24 21:24:32 +0900

    test: add sol50 test

---
+++ test_solutions/test_50.py
@@ -0,0 +1,35 @@
+import pytest
+from solutions.sol_50 import Solution
+
+cases = [
+    {
+        'input': {
+            'x': 2.00000,
+            'n': 10,
+        },
+        'output': 1024.00000,
+    },
+    {
+        'input': {
+            'x': 2.10000,
+            'n': 3,
+        },
+        'output': 9.26100,
+    },
+    {
+        'input': {
+            'x': 2.00000,
+            'n': -2,
+        },
+        'output': 0.25000,
+    },
+]
+
+
+@pytest.mark.sol50
+def test_run():
+    for case in cases:
+        assert round(Solution.my_pow(
+            x=case['input']['x'],
+            n=case['input']['n'],
+        ), 5) == case['output']
