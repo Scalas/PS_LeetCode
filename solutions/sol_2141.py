d11fd41c95ba67ff6f607908a4321e3e0860dc27
Scala <scala0114@gmail.com>
2023-07-28 01:02:56 +0900

    feat: solve +1 problem

---
+++ solutions/sol_2141.py
@@ -0,0 +1,39 @@
+from typing import List
+from heapq import heappush, heappop
+
+
+class Solution:
+    @staticmethod
+    def max_run_time(n: int, batteries: List[int]) -> int:
+        m = len(batteries)
+        batteries.sort(reverse=True)
+        cand = batteries[:n]
+        remain =  batteries[n:]
+
+        def check(time):
+            plus = [(0, num) for num in remain]
+            for num in cand:
+                while num < time:
+                    if not plus:
+                        return False
+                    start, count = heappop(plus)
+                    if start > num:
+                        return False
+                    borrow = min(count, time - num)
+                    start += borrow
+                    count -= borrow
+                    if count:
+                        heappush(plus, (start, count))
+                    num += borrow
+            return True
+
+        s, e = min(batteries), sum(batteries) // n
+        answer = 0
+        while s <= e:
+            mid = (s + e) // 2
+            if check(mid):
+                answer = mid
+                s = mid + 1
+            else:
+                e = mid - 1
+        return answer
---
+++ test_solutions/test_2141.py
@@ -0,0 +1,35 @@
+import pytest
+from solutions.sol_2141 import Solution
+
+cases = [
+    {
+        'input': {
+            'n': 2,
+            'batteries': [3, 3, 3]
+        },
+        'output': 4,
+    },
+    {
+        'input': {
+            'n': 2,
+            'batteries': [1, 1, 1, 1]
+        },
+        'output': 2,
+    },
+    {
+        'input': {
+            'n': 3,
+            'batteries': [10, 10, 3, 5]
+        },
+        'output': 8,
+    },
+]
+
+
+@pytest.mark.sol_2141
+def test_run():
+    for case in cases:
+        assert Solution.max_run_time(
+            n=case['input']['n'],
+            batteries=case['input']['batteries'],
+        ) == case['output']
