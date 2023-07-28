b5a52c550f78d25bab132959a6fe5c0fd0a4512d
Scala <scala0114@gmail.com>
2023-07-28 19:31:17 +0900

    feat: solve +1 problem

---
+++ solutions/sol_486.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def predict_the_winner(nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [[-1] * n for _ in range(n)]
+
+        def dfs(s, e, turn):
+            if s > e:
+                return 0
+            if dp[s][e] == -1:
+                if turn:
+                    dp[s][e] = min(dfs(s, e - 1, 1 - turn), dfs(s + 1, e, 1 - turn))
+                else:
+                    dp[s][e] = max(dfs(s, e - 1, 1 - turn) + nums[e], dfs(s + 1, e, 1 - turn) + nums[s])
+            return dp[s][e]
+        
+        return dfs(0, n - 1, 0) * 2 - sum(nums) >= 0
---
+++ test_solutions/test_486.py
@@ -0,0 +1,25 @@
+import pytest
+from solutions.sol_486 import Solution
+
+cases = [
+    {
+        'input': {
+            'nums': [1, 5, 2],
+        },
+        'output': False,
+    },
+    {
+        'input': {
+            'nums': [1, 5, 233, 7],
+        },
+        'output': True,
+    },
+]
+
+
+@pytest.mark.sol_486
+def test_run():
+    for case in cases:
+        assert Solution.predict_the_winner(
+            nums=case['input']['nums']
+        ) == case['output']
