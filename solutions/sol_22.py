cc0b1c8e091f982c16862efbd75a5f7e9034f878
Scala <scala0114@gmail.com>
2023-07-29 14:16:24 +0900

    feat: solve +1 problem

---
+++ solutions/sol_22.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def generate_parenthesis(n: int) -> List[str]:
+        answer = []
+
+        def dfs(l, r, st):
+            if l == r == 0:
+                answer.append(''.join(st))
+            if l:
+                st.append('(')
+                dfs(l - 1, r, st)
+                st.pop()
+            if r and l < r:
+                st.append(')')
+                dfs(l, r - 1, st)
+                st.pop()
+
+        dfs(n, n, [])
+        return answer
---
+++ test_solutions/test_22.py
@@ -0,0 +1,26 @@
+import pytest
+
+from solutions.sol_22 import Solution
+
+cases = [
+    {
+        'input': {
+            'n': 3,
+        },
+        'output': ["((()))", "(()())", "(())()", "()(())", "()()()"],
+    },
+    {
+        'input': {
+            'n': 1,
+        },
+        'output': ["()"],
+    },
+]
+
+
+@pytest.mark.sol22
+def test_run():
+    for case in cases:
+        assert Solution.generate_parenthesis(
+            n=case['input']['n'],
+        ) == case['output']
