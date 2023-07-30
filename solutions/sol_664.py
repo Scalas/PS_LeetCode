927db52cb1123064d846c21e17bd6fc6006ea109
Scala <scala0114@gmail.com>
2023-07-30 15:26:36 +0900

    feat: solve +1 problem

---
+++ solutions/sol_664.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+
+class Solution:
+    @staticmethod
+    def strange_printer(s: str) -> int:
+        st = []
+        for c in s:
+            if st and st[-1] == c:
+                continue
+            st.append(c)
+        s = ''.join(st)
+        n = len(s)
+        idx = defaultdict(list)
+        for i in range(n):
+            idx[s[i]].append(i)
+        dp = [[[-1] * 27 for _ in range(n)] for _ in range(n)]
+
+        def dfs(l, r, bg):
+            if l > r:
+                return 0
+            ch = ord(s[l]) - ord('a') + 1
+            if l == r:
+                return 1 if ch != bg else 0
+            if dp[l][r][bg] == -1:
+                res = 101
+                if s[l] == s[r]:
+                    res = dfs(l + 1, r - 1, ch) + (1 if ch != bg else 0)
+                else:
+                    for mid in idx[s[l]]:
+                        if mid >= r:
+                            break
+                        if mid < l:
+                            continue
+                        res = min(res, dfs(l, mid, bg) + dfs(mid + 1, r, bg))
+                dp[l][r][bg] = res
+            return dp[l][r][bg]
+        return dfs(0, n - 1, 0)
---
+++ test_solutions/test_664.py
@@ -0,0 +1,37 @@
+import pytest
+from solutions.sol_664 import Solution
+
+cases = [
+    {
+        'input': {
+            's': 'aaabbb',
+        },
+        'output': 2,
+    },
+    {
+        'input': {
+            's': 'aba',
+        },
+        'output': 2,
+    },
+    {
+        'input': {
+            's': 'bacdadacbdbcabd',
+        },
+        'output': 9,
+    },
+    {
+        'input': {
+            's': 'xfqsduwlrrbvtglkdjvplhgtwmjkocjomwkamntkpskipyshnnsivshulwsebzswogvjtevsmjxrfgyxhsvfnptytkwaguvsiqqu',
+        },
+        'output': 73,
+    },
+]
+
+
+@pytest.mark.sol_664
+def test_run():
+    for case in cases:
+        assert Solution.strange_printer(
+            s=case['input']['s'],
+        ) == case['output']
