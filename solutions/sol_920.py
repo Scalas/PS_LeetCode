791a318c642f6abd1ec2151b82c71c4b898e3ba9
Scala <scala0114@gmail.com>
2023-08-06 17:58:10 +0900

    feat: solve +1 problem

---
+++ solutions/sol_920.py
@@ -0,0 +1,12 @@
+class Solution:
+    @staticmethod
+    def num_music_playlists(n: int, goal: int, k: int) -> int:
+        m = goal
+        mod = 10 ** 9 + 7
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+        for i in range(1, n + 1):
+            for j in range(i, m + 1):
+                dp[i][j] = (dp[i - 1][j - 1] * (n - i + 1) + dp[i][j - 1] * (i - min(j - 1, k))) % mod
+        return dp[n][m]
---
+++ test_solutions/test_920.py
@@ -0,0 +1,55 @@
+import pytest
+from solutions.sol_920 import Solution
+
+cases = [
+    {
+        'input': {
+            'n': 3,
+            'goal': 3,
+            'k': 1,
+        },
+        'output': 6
+    },
+    {
+        'input': {
+            'n': 2,
+            'goal': 3,
+            'k': 0,
+        },
+        'output': 6
+    },
+    {
+        'input': {
+            'n': 2,
+            'goal': 3,
+            'k': 1,
+        },
+        'output': 2
+    },
+    {
+        'input': {
+            'n': 2,
+            'goal': 4,
+            'k': 0,
+        },
+        'output': 14
+    },
+    {
+        'input': {
+            'n': 3,
+            'goal': 4,
+            'k': 0,
+        },
+        'output': 36
+    },
+]
+
+
+@pytest.mark.sol920
+def test_run():
+    for case in cases:
+        assert Solution.num_music_playlists(
+            n=case['input']['n'],
+            goal=case['input']['goal'],
+            k=case['input']['k'],
+        ) == case['output']
