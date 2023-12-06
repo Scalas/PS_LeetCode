0c8793be59b126ab7938295b18a67cbe6fcc37e0
Scala <scala0114@gmail.com>
2023-12-06 21:29:16 +0900

    feat: solve +1 problem

---
+++ solutions/sol_1716.py
@@ -0,0 +1,14 @@
+class Solution:
+    @staticmethod
+    def total_money(n: int) -> int:
+        r = n // 7
+        rem = n % 7
+        base = 28
+        answer = 0
+
+        cnt = 0
+        for _ in range(r):
+            answer += (base + cnt * 7)
+            cnt += 1
+        answer += ((1 + rem) * rem // 2) + cnt * rem
+        return answer
---
+++ test_solutions/test_1716.py
@@ -0,0 +1,34 @@
+import pytest
+from solutions.sol_1716 import Solution
+
+cases = [
+    {
+        "input": {
+            "n": 4,
+        },
+        "output": 10,
+    },
+    {
+        "input": {
+            "n": 10,
+        },
+        "output": 37,
+    },
+    {
+        "input": {
+            "n": 20,
+        },
+        "output": 96,
+    },
+]
+
+
+@pytest.mark.sol1716
+def test_run():
+    for case in cases:
+        assert (
+                Solution.total_money(
+                    n=case["input"]["n"],
+                )
+                == case["output"]
+        )
