561cf06b229bbca2dd202445a5a7d1a7652e83a0
scala0114 <scala.jeong@bucketplace.net>
2024-08-07 21:11:22 +0900

    test: add test

---
+++ test_solutions/test_273.py
@@ -0,0 +1,35 @@
+import pytest
+from solutions.sol_273 import Solution
+
+
+cases = [
+    {
+        "input": {
+            "num": 123
+        },
+        "output": "One Hundred Twenty Three",
+    },
+    {
+        "input": {
+            "num": 12345
+        },
+        "output": "Twelve Thousand Three Hundred Forty Five",
+    },
+    {
+        "input": {
+            "num": 1234567
+        },
+        "output": "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven",
+    },
+]
+
+
+@pytest.mark.sol273
+def test_run():
+    for case in cases:
+        assert (
+            Solution.number_to_words(
+                num=case["input"]["num"],
+            )
+            == case["output"]
+        )
