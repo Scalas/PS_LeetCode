103290ee6e0f9c97ae801c40a1a865166ce2f70e
scala0114 <scala.jeong@bucketplace.net>
2025-01-24 18:40:22 +0900

    refactor: update +1 problem

--- solutions/sol_802.py
+++ solutions/sol_802.py
@@ -4,23 +4,24 @@ from typing import List
 class Solution:
     @staticmethod
     def eventual_safe_nodes(graph: List[List[int]]) -> List[int]:
-        graph = list(map(lambda x: set(x), graph))
         n = len(graph)
-        reverse_graph = [set() for _ in range(n)]
+        rg = [[] for _ in range(n)]
+        degree = [0] * n
         for i in range(n):
-            for nxt in graph[i]:
-                reverse_graph[nxt].add(i)
-
+            for j in graph[i]:
+                rg[j].append(i)
+                degree[i] += 1
         q = [i for i in range(n) if not graph[i]]
-        answer = []
+
+        answer = q[:]
         while q:
             nq = []
             for cur in q:
-                answer.append(cur)
-                for pre in reverse_graph[cur]:
-                    graph[pre].remove(cur)
-                    if not graph[pre]:
-                        nq.append(pre)
+                for before in rg[cur]:
+                    degree[before] -= 1
+                    if degree[before] == 0:
+                        nq.append(before)
+                        answer.append(before)
             q = nq
         answer.sort()
         return answer
